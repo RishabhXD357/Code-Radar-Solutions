76268c6e2c257db773898a86278b1d7ff9f61b85
RishabhXD357 <rishabh0690.becse24@chitkara.edu.in>
2025-01-30 12:43:17 +0530

    Solution to Logical AND Operation

---
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+typedef int rishabh;
+int main() {
+    rishabh a,b;
+    if(a>0 && b>0){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
